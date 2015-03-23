d37cb12fd527dbe2957ef3b5c216dfebdd2afddb
p2pKernel <xujy@fun.tv>
2015-03-23 14:39:27 +0800

    add playinfo interface

--- BesTVQoS/BesTVQoS/tplay/models.py
+++ BesTVQoS/BesTVQoS/tplay/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class playinfo(models.Model):
+    ServiceType=models.CharField(max_length=30)
+    DeviceType=models.CharField(max_length=255)
+    ISP=models.CharField(max_length=255)
+    Area=models.CharField(max_length=255)
+    ViewType=models.IntegerField()
+    Date=models.DateField()
+    Hour=models.IntegerField()    
+    Records=models.IntegerField()
+    Users=models.IntegerField()
+    AvgTimeOfUser=models.IntegerField()
+
+    class Meta:
+        db_table="playinfo"
+        unique_together=('ServiceType', 'DeviceType', 'ISP', 'Area', 'ViewType', 'Date', 'Hour')
--- BesTVQoS/BesTVQoS/tplay/views.py
+++ BesTVQoS/BesTVQoS/tplay/views.py
@@ -1,1 +1,38 @@
-# Create your views here.
+# -*- coding: utf-8 -*- 
+
+import json
+
+from django.http import HttpResponse
+from tplay.models import *
+
+logger = logging.getLogger("django.request")
+
+def playinfo(request):
+    result="ok"
+    if request.method=="POST":
+        try:
+            contents=json.loads(request.body)
+            for item in contents:
+                create_date='%s-%s-%s'%(item['date'][0:4], item['date'][4:6], item['date'][6:8])
+                playinfo_obj=playinfo(
+                    ServiceType=item['servicetype'],
+                    DeviceType=item['dev'],
+                    ISP=item['ISP'],
+                    Area=item['Area'],
+                    ViewType=item['viewtype'],
+                    Date=create_date,
+                    Hour=item['hour'],
+                    Records=item['records'],
+                    Users=item['users'],
+                    AvgTimeOfUser=item['avgtime'])
+                playinfo_obj.save(force_update=true)
+        except ValueError, e:
+            result="error: %s"%e
+        except Exception, e:
+            result="error: %s"%e
+    else:
+        result="error"
+
+    respStr=json.dumps({"result":result})
+    logger.debug("update_playinfo: %s"%(respStr))
+    return HttpResponse(respStr, content_type="application/json")
