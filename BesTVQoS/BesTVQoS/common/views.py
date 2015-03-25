bb803407013fe4b8f4693840f24b8b92301ee52d
xujy <xujy@fun.tv>
2015-03-26 00:30:33 +0800

    add cookie

--- BesTVQoS/BesTVQoS/common/views.py
+++ BesTVQoS/BesTVQoS/common/views.py
@@ -10,6 +10,7 @@ from django.shortcuts import render_to_response
 from django.template import Context
 from django.db import connection
 from navi import Navi
+from date_time_tool import today
 
 logger = logging.getLogger("django.request")
 
@@ -58,6 +59,9 @@ def get_device_types1(table, service_type, begin_date, end_date, cu=None):
     for item in cu.fetchall():
         device_types.append(item[0].encode('utf-8'))
 
+    if len(device_types) == 0:
+        device_types = ['']
+
     return device_types
 
 def get_device_types(table, service_type, begin_date, end_date, cu=None):
@@ -71,7 +75,7 @@ def get_device_types(table, service_type, begin_date, end_date, cu=None):
         for i in q:
             device_types.append(i["DeviceType"])
     else:
-        device_types=[""]
+        device_types=[]
     return device_types
 
 def get_device_type(request, dev=""):
@@ -84,10 +88,14 @@ def get_device_type(request, dev=""):
             url = request.META.get('HTTP_REFERER')
 
             table_name = ""
-            if url.find("playing") != -1:
+            if url.find("playing_trend") != -1:
                 table_name = "playinfo"
             elif url.find("fbuffer") != -1:
                 table_name = "fbuffer"
+            elif url.find("play_time") != -1:
+                table_name = "playtime"
+            elif url.find("fluency") != -1:
+                table_name = "fluency"
 
             device_types = get_device_types1(
                 table_name, service_type, begin_date, end_date)
@@ -97,3 +105,13 @@ def get_device_type(request, dev=""):
             raise e
 
     return HttpResponse(respStr, content_type="text/json")
+
+def get_default_values_from_cookie(request):
+    if "filters" not in request.COOKIES:
+        return json.loads('{"st":"All","dt":"","begin":"%s","end":"%s"}'%(str(today()), today()))
+    else:
+        return json.loads(request.COOKIES["filters"])
+        
+def set_default_values_to_cookie(response, context):
+    response.set_cookie("filters", json.dumps({"st": context["default_service_type"], "dt": context["default_device_type"],
+                                    "begin": context['default_begin_date'], "end":context['default_end_date']}), max_age=30000)
