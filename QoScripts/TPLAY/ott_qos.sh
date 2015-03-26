96069dc2cd30f1dad9186966ec0e2936cc5be36e
U-FUNSHION\dingliang <dingliang@FX-BJ618.funshion.com>
2015-03-26 14:45:47 +0800

    add files for BesTV QoS

--- QoScripts/TPLAY/ott_qos.sh
+++ QoScripts/TPLAY/ott_qos.sh
@@ -37,7 +37,12 @@ python update_data_into_db_date.py ${svrtype} ${dev} ${date} ${hour} 5 ${datadir
 # for all day
 if [ ${hour} -eq 24 ]; then
 	perl playtime_by_usr.pl ${date} ${savedir} ${filename}
-	
 	python update_playprofile_into_db_date.py ${svrtype} ${dev} ${date} ${datadir}/playtm_by_usr
+	
+	# for BesTV
+	perl BesTV_fbuffer_cdf_by_day.pl ${date} ${savedir} ${filename}
+	perl BesTV_pchoke_ratio_by_day.pl ${date} ${savedir} ${filename}
+	
+	python BesTV_update_data_into_db_date.py ${svrtype} ${dev} ${date} ${datadir}/BesTV_fbuffer_3s_ratio_data ${datadir}/BesTV_avg_pchoke_data
 fi
 
