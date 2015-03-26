bf6079985c0d3ab4ea8485bfea01a8f2552c2a93
U-FUNSHION\dingliang <dingliang@FX-BJ618.funshion.com>
2015-03-26 10:32:50 +0800

    change something

--- QoScripts/TPLAY/ott_start.sh
+++ QoScripts/TPLAY/ott_start.sh
@@ -2,11 +2,11 @@
 
 export PYTHONPATH=/usr/bin/python:/usr/bin/python2.6:/usr/lib/python2.6/site-packages
 
-date=$(date +"%Y%m%d")
-#date=$1 
+#date=$(date +"%Y%m%d")
+date=$1 
 
-hour=$(date -d -1hour +%H)
-#hour=$2 
+#hour=$(date -d -1hour +%H)
+hour=$2 
 
 bash prepare_for_qos.sh ${date} ${hour}
 
