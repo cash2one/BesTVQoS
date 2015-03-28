ba7fa59b86996a496e47449f06f2cfa45a34099b
bestv <p2pKernel@fun.tv>
2015-03-28 11:33:28 +0800

    try 5 times for FTP-downloading

--- QoScripts/TPLAY/download_files.sh
+++ QoScripts/TPLAY/download_files.sh
@@ -3,12 +3,17 @@
 date=$1
 hour=$2
 
-ftp -n -v<<!
-open 124.108.10.55
-user ott_ts_tplay ott_ts_tplay
-binary
-prompt
-mget TPLAY_${date}${hour}*_jsdx_.zip
-close
-bye
-!
+MAX_TRY=5
+i=0
+
+while(($i<$MAX_TRY))
+do
+	bash download_files_by_FTP.sh ${date} ${hour}
+
+	if [ -e TPLAY_${date}${hour}55_jsdx_.zip ]; then
+		break
+	fi
+
+	i=$(($i+1))
+	sleep 60	
+done
