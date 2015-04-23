e110a9e62ce81bf6e8c69025b39b48b7035d4933
bestv <p2pKernel@fun.tv>
2015-04-23 20:17:25 +0800

    change something

--- QoScripts/ServerQoS/service/qos_start_by_day.sh
+++ QoScripts/ServerQoS/service/qos_start_by_day.sh
@@ -2,8 +2,8 @@
 
 export PYTHONPATH=/usr/bin/python:/usr/bin/python2.6:/usr/lib/python2.6/site-packages
 
-#date=$(date -d -1hour +"%Y%m%d")
-date=$1
+date=$(date -d yesterday +"%Y%m%d")
+#date=$1
 
-bash server_qos_start_by_day.sh ${date} 24 B2B
+bash server_qos_start_by_day.sh ${date} 24 B2C
 
