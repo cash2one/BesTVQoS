35d99630c8b37ed9484477dea1bb0cb487c451ca
bestv <p2pKernel@fun.tv>
2015-04-23 20:16:12 +0800

    merge

--- QoScripts/ServerQoS/service/server_qos_start_by_day.sh
+++ QoScripts/ServerQoS/service/server_qos_start_by_day.sh
@@ -7,7 +7,7 @@ type=$3
 mkdir -p tmp/${type}/${date}
 mkdir -p ${type}/${date}
 	
-perl filter_data_by_svrip.pl ${type} ${date} ${hour} svrip 0 tmp/${type}/${date}/*_all
+perl filter_data_by_svrip.pl ${type} ${date} ${hour} svrip 1 tmp/${type}/${date}/*_all
 
 bash server_qos_by_svrip.sh ${type} ${date} ${hour} ${type}/${date}/distribution_data_svrip_${hour}
 
