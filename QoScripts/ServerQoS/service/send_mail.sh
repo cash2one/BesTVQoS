4c2fad84050c29edb53035346ab7a558e2dc540e
bestv <p2pKernel@fun.tv>
2015-05-08 15:41:43 +0800

    add files: data2html

--- QoScripts/ServerQoS/service/send_mail.sh
+++ QoScripts/ServerQoS/service/send_mail.sh
@@ -9,17 +9,17 @@ echo "|服务器类型|服务器IP|全天|200 OK占比|请求时长P25(s)|请求
 
 awk '{split(FILENAME, b, "/");if(index($0, "|24|"))printf("|%s%s\n", b[4], $0)}' B2C/${date}/*/*/response_data_CDF_5 | sort -t "|" -k11,11 -nr >> req_by_svrip_${date}
 
-perl xml2html_channel_statistics.pl serverlist.info req_by_svrip_${date}.html req_by_svrip_${date}
+perl xml2html_serverlist.pl serverlist.info req_by_svrip_${date}.html req_by_svrip_${date}
 
 echo "|服务器类型|服务器IP|全天|200 OK占比|响应时长P25(s)|响应时长P50(s)|响应时长P75(s)|响应时长P90(s)|响应时长P95(s)|响应请求时长(s)|记录数|" > resp_by_svrip_${date}
 
 awk '{split(FILENAME, b, "/");if(index($0, "|24|"))printf("|%s%s\n", b[4], $0)}' B2C/${date}/*/*/response_data_CDF_4 | sort -t "|" -k11,11 -nr >> resp_by_svrip_${date}
 
-perl xml2html_channel_statistics.pl serverlist.info resp_by_svrip_${date}.html resp_by_svrip_${date}
+perl xml2html_serverlist.pl serverlist.info resp_by_svrip_${date}.html resp_by_svrip_${date}
 
-perl xml2html.pl serverlist.info code_detail_by_svrip_${date}.html code B2C/${date}/*/*/distribution_data_code_24
+perl xml2html_by_server.pl serverlist.info code_detail_by_svrip_${date}.html code B2C/${date}/*/*/distribution_data_code_24
 
-perl xml2html.pl serverlist.info url_detail_by_svrip_${date}.html url B2C/${date}/*/*/response_data_CDF_by_url_5_24
+perl xml2html_by_server.pl serverlist.info url_detail_by_svrip_${date}.html url B2C/${date}/*/*/response_data_CDF_by_url_5_24
 
 python send_mail_statistic_by_svrip.py ${date} 1.0
 
