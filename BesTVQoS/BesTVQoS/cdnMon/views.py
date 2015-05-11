cc8b786c72130dea69b9055c674928266e93c719
xujy <xujy@fun.tv>
2015-05-11 19:45:09 +0800

    Refactor code by PEP8

--- BesTVQoS/BesTVQoS/cdnMon/views.py
+++ BesTVQoS/BesTVQoS/cdnMon/views.py
@@ -1,9 +1,8 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import logging
 import MySQLdb
 import json
-from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.db import connection
 from common.views import HtmlTable
@@ -12,32 +11,35 @@ from common.date_time_tool import current_time
 
 logger = logging.getLogger("django.request")
 
+
 def show_ms_error(request, dev=""):
     context = {}
 
     date = request.GET.get("date", str(get_day_of_day(-1)))
 
     table = HtmlTable()
-    table.mtitle = u"ms_error信息"
-    table.mheader = [u"响应码", "ClientIP", u"省份", u"运营商", 'ServerIP', u'省份', u'运营商', u'次数', 'url']
+    table.mtitle = "ms_error信息"
+    table.mheader = ["响应码", "ClientIP", "省份", "运营商", 
+        'ServerIP', '省份', '运营商', '次数', 'url']
     table.msub = []
 
-    sql = "select Resp, ClientIP, ClientISP, ClientArea, ServIP, ServISP, ServArea, Count, URL  \
+    sql = "select Resp, ClientIP, ClientISP, ClientArea, ServIP, \
+            ServISP, ServArea, Count, URL  \
             from ms_error_info where Date='%s'" % date
 
     logger.debug("Server List SQL - %s" % sql)
 
-    cu = connection.cursor()
+    mysql_cur = connection.cursor()
     begin_time = current_time()
-    cu.execute(sql)
-    results = cu.fetchall()
+    mysql_cur.execute(sql)
+    results = mysql_cur.fetchall()
     logger.info("execute sql:  %s, cost: %s" % (sql, (current_time() - begin_time)))
     subs = []
     for row in results:
         sub = []
         for i in range(8):
             sub.append(row[i])
-        sub.append('''<a href="%s" target="_blank">%s</a>'''%(row[8], row[8]))
+        sub.append('''<a href="%s" target="_blank">%s</a>''' % (row[8], row[8]))
         subs.append(sub)
 
     table.msub = subs
@@ -47,14 +49,15 @@ def show_ms_error(request, dev=""):
 
     return render_to_response('show_ms_error.html', context)
 
+
 def show_tsdelay(request, dev=""):
     context = {}
 
     date = request.GET.get("date", str(get_day_of_day(-1)))
 
     table = HtmlTable()
-    table.mtitle = u"CDN信息"
-    table.mheader = ["ServerIP", u"省份", u"运营商", u'流量(G)', u'内网流量占比(%)', '详情']
+    table.mtitle = "CDN信息"
+    table.mheader = ["ServerIP", "省份", "运营商", '流量(G)', '内网流量占比(%)', '详情']
     table.msub = []
 
     sql = "select ServIP, ServArea, ServISP, Flow, InnerFlow, ServiceType \
@@ -62,19 +65,20 @@ def show_tsdelay(request, dev=""):
 
     logger.debug("Server List SQL - %s" % sql)
 
-    cu = connection.cursor()
+    mysql_cur = connection.cursor()
     begin_time = current_time()
-    cu.execute(sql)
-    results = cu.fetchall()
+    mysql_cur.execute(sql)
+    results = mysql_cur.fetchall()
     logger.info("execute sql:  %s, cost: %s" % (sql, (current_time() - begin_time)))
     subs = []
     for row in results:
         sub = []
         for i in range(3):
             sub.append(row[i])
-        sub.append("%.3f"%(float(row[3])/1024/1024/1024))
-        sub.append("%.1f"%(float(row[4]*1.0/row[3]*100)))
-        sub.append(u'''<a href="/show_cdn_detail?ip=%s&date=%s&servicetype=%s" target="main">详情</a>'''%(row[0], date, row[5]))
+        sub.append("%.3f" % (float(row[3])/1024/1024/1024))
+        sub.append("%.1f" % (float(row[4]*1.0/row[3]*100)))
+        sub.append('''<a href="/show_cdn_detail?ip=%s&date=%s&servicetype=%s" \
+            target="main">详情</a>''' % (row[0], date, row[5]))
         subs.append(sub)
     table.msub = subs
 
@@ -83,24 +87,26 @@ def show_tsdelay(request, dev=""):
 
     return render_to_response('show_tsdelay.html', context)
 
+
 # 广东+电信+4906156100+11976708_福建+电信+3268066792+10235635
-def make_pie_items(ip, area, isp, info):
-    pie_items=[]
-    pie_item={}
+def make_pie_items(info):
+    pie_items = []
+    pie_item = {}
     pie_item["index"] = 0
-    pie_item['title']=u'流量分布（%）'
-    pie_item['subtitle']='单位(G)'
+    pie_item['title'] = '流量分布（%）'
+    pie_item['subtitle'] = '单位(G)'
 
     #flows
-    items=info.split("_")
-    flows=[]
+    items = info.split("_")
+    flows = []
     for i in items:
-        subitems=i.split("+")
-        flow_item=[]
-        key='%s%s'%(subitems[0], subitems[1])
+        subitems = i.split("+")
+        flow_item = []
+        key = '%s%s' % (subitems[0], subitems[1])
         flow_item.append(key)
-        f='%.3f'%(float(subitems[2])/1024/1024/1024)
-        flow_item.append(float(f))
+        percent = '%.3f' % (float(subitems[2])/1024/1024/1024)
+        flow_item.append(float(percent))
+
         flows.append(flow_item)
 
     series = []
@@ -109,113 +115,121 @@ def make_pie_items(ip, area, isp, info):
             name: '占比',
             data: %s
         }''' % (json.dumps(flows))
-    series.append(serie_item)        
+    series.append(serie_item)
 
     pie_item["series"] = ",".join(series)
     pie_items.append(pie_item)
     return pie_items
 
-def make_rates(area, isp, info, tflow, percent):
-    flows=[]
-    items=info.split("_")
-    for i in items:
-        subitems=i.split("+")
-        if float(subitems[2])/tflow < percent:
-            break;
 
-        flow_item={}
-        flow_item["server_name"]="%s%s"%(area, isp)
-        flow_item["client_name"]="%s%s"%(subitems[0], subitems[1])
-        flow_item["rate"]="%d"%(float(subitems[2])/float(subitems[3]))
+def make_rates(area, isp, info, tflow, percent):
+    flows = []
+    items = info.split("_")
+    for i in items:
+        subitems = i.split("+")
+        if float(subitems[2])/tflow < percent:
+            break
+
+        flow_item = {}
+        flow_item["server_name"] = "%s%s" % (area, isp)
+        flow_item["client_name"] = "%s%s" % (subitems[0], subitems[1])
+        flow_item["rate"] = "%d" % (float(subitems[2])/float(subitems[3]))
         flows.append(flow_item)
     return flows
+
 
 def make_flows(info, tflow, percent):
-    flows=[]
-    items=info.split("_")
+    flows = []
+    items = info.split("_")
     for i in items:
-        subitems=i.split("+")
+        subitems = i.split("+")
         if float(subitems[2])/tflow < percent:
-            break;
-        flow_item={}
-        flow_item["client_name"]='%s%s  %.1f'%(subitems[0], subitems[1], float(subitems[2])/tflow*100)
-        flow_item["flow"]="%d"%(float(subitems[2])/float(subitems[3]))
+            break
+        flow_item = {}
+        flow_item["client_name"] = '%s%s  %.1f' % (subitems[0], subitems[1], 
+            float(subitems[2])/tflow*100)
+        flow_item["flow"] = "%d" % (float(subitems[2])/float(subitems[3]))
         flows.append(flow_item)
     return flows
 
+
 def get_geo_from_db(area):
-    db = MySQLdb.connect('localhost', 'root', 'funshion', 'BesTVQoS')
-    db.set_character_set('utf8')  
-    cursor = db.cursor() 
-    select_sql="select Jing, Wei from province_geo where Province='%s'"%(area)
+    mysql_db = MySQLdb.connect('localhost', 'root', 'funshion', 'BesTVQoS')
+    mysql_db.set_character_set('utf8')
+    cursor = mysql_db.cursor()
+    select_sql = "select Jing, Wei from province_geo where Province='%s'" \
+        % (area)
     cursor.execute(select_sql)
     record = cursor.fetchone()
     if record is not None:
-        return "%s,%s"%(record[0], record[1])
+        return "%s,%s" % (record[0], record[1])
     return "1,1"
 
+
 def get_china_geos(area, isp, info, tflow, percent):
-    geos=[]
-    subkey='%s%s'%(area, isp)
-    item={}
-    item["area"]=subkey
-    item["geo"]=get_geo_from_db(area)
+    geos = []
+    subkey = '%s%s' % (area, isp)
+    item = {}
+    item["area"] = subkey
+    item["geo"] = get_geo_from_db(area)
     geos.append(item)
 
-    items=info.split("_")
-    count=0
+    items = info.split("_")
     for i in items:
-        subitems=i.split("+")
+        subitems = i.split("+")
         if float(subitems[2])/tflow < percent:
-            break;
+            break
 
-        subkey="%s%s"%(subitems[0], subitems[1])
+        subkey = "%s%s" % (subitems[0], subitems[1])
         if subkey not in geos:
-            item={}
-            item["area"]=subkey
-            item["geo"]=get_geo_from_db(subitems[0])
+            item = {}
+            item["area"] = subkey
+            item["geo"] = get_geo_from_db(subitems[0])
             geos.append(item)
-        subkey2='%s%s  %.1f'%(subitems[0], subitems[1], float(subitems[2])/tflow*100)
+        subkey2 = '%s%s  %.1f' % (subitems[0], subitems[1], 
+            float(subitems[2])/tflow*100)
         if subkey2 not in geos:
-            item={}
-            item["area"]=subkey2
-            item["geo"]=get_geo_from_db(subitems[0])
+            item = {}
+            item["area"] = subkey2
+            item["geo"] = get_geo_from_db(subitems[0])
             geos.append(item)
     return geos
+
 
 def show_cdn_detail(request, dev=""):
     context = {}
     date = request.GET.get("date", str(get_day_of_day(-1)))
-    servicetype=request.GET.get("servicetype", 'B2B')
-    ip = request.GET.get("ip", "127.0.0.1")
+    servicetype = request.GET.get("servicetype", 'B2B')
+    servip = request.GET.get("ip", "127.0.0.1")
     sql = "select ServArea, ServISP, Flow, InnerFlow, Info \
-            from ts_delay where ServIP='%s' and ServiceType='%s' and Date='%s'" % (ip, servicetype, date)
+            from ts_delay where ServIP='%s' and ServiceType='%s' \
+            and Date='%s'" % (servip, servicetype, date)
 
-    cu = connection.cursor()
+    mysql_cur = connection.cursor()
     begin_time = current_time()
-    cu.execute(sql)
-    results = cu.fetchall()
+    mysql_cur.execute(sql)
+    results = mysql_cur.fetchall()
     logger.info("execute sql:  %s, cost: %s" % (sql, (current_time() - begin_time)))
-    if len(results)<=0:
+    if len(results) <= 0:
         return render_to_response('bestv_servers_map.html', context)
 
-    row=results[0]
-    area=row[0]
-    isp=row[1]
-    tflow=row[2]
-    context['title']=u'服务器信息: 速率KBps'
-    context['subtitle']=u'%s-%s-%s'%(area, isp, date)
-    context['legendTxt']=ip
+    row = results[0]
+    area = row[0]
+    isp = row[1]
+    tflow = row[2]
+    context['title'] = '服务器信息: 速率KBps'
+    context['subtitle'] = '%s-%s-%s' % (area, isp, date)
+    context['legendTxt'] = servip
 
-    item={}
-    item["title"]=u'%s-%s-%s'%(ip, area, isp)
-    info=row[4]
-    context["pie_contents"]=make_pie_items(ip, area, isp, info)
+    item = {}
+    item["title"] = '%s-%s-%s' % (servip, area, isp)
+    info = row[4]
+    context["pie_contents"] = make_pie_items(info)
 
     # maps
-    item["geos"]=get_china_geos(area, isp, info, tflow, 0.02)
-    item["rates"]=make_rates(area, isp, info, tflow, 0.02)
-    item["flows"]=make_flows(info, tflow, 0.02)
+    item["geos"] = get_china_geos(area, isp, info, tflow, 0.02)
+    item["rates"] = make_rates(area, isp, info, tflow, 0.02)
+    item["flows"] = make_flows(info, tflow, 0.02)
 
-    context['item']=item
+    context['item'] = item
     return render_to_response('bestv_servers_map.html', context)
