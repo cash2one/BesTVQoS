a0664b960bae6e372471cd1129a9ab82d5c92d09
lixl1 <lixl1@fun.tv>
2015-05-13 18:33:49 +0800

    set expire time of DeviceType/Version cache to 20 minus if begin date or end date is today.

--- BesTVQoS/BesTVQoS/common/views.py
+++ BesTVQoS/BesTVQoS/common/views.py
@@ -142,6 +142,8 @@ def cache_types(table, begin_date, end_date, type_name, types_list):
         base_date = todaystr()
         if cmp(begin_date, base_date) < 0 and cmp(end_date, base_date) < 0:
             redis_cache.expire(types_key, 3600*2)
+        else:
+            redis_cache.expire(types_key, 1500)
         logger.info("cache device types suc: %s" % len(types_list))
     except Exception, e:
         logger.info("cache deivce types fail: %s" % e)
