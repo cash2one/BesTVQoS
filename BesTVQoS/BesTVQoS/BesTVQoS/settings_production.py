bfe85144a912dcd67a51690f60c2666aa2fcbc0c
lixl1 <lixl1@fun.tv>
2015-06-02 17:17:12 +0800

    update uwsgi configuration.

--- BesTVQoS/BesTVQoS/BesTVQoS/settings_production.py
+++ BesTVQoS/BesTVQoS/BesTVQoS/settings_production.py
@@ -4,4 +4,10 @@ Django product settings for BesTVQoS project.
 
 from settings import *
 
-DEBUG = TEMPLATE_DEBUG = False
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = (
+    'bestvwatch.chinacloudapp.cn',
+    'localhost',
+)
